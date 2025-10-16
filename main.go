d757dead39dd9b6ab2267ca0c20013fa6eb29622
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-10-16 19:05:05 +0600

    Package

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"example.com/mathlib"
+)
+
+var(
+	a=10
+	b=20
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Showing custom package")
+	mathlib.Add(a,b)
 }
---
+++ mathlib/math.go
@@ -0,0 +1,6 @@
+package mathlib
+
+func Add(x int, y int) {
+	z := x + y
+	println("Addition is:", z)
+}
