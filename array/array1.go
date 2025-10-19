af0d4e3be6f61e6e20706d1faa12215b3c4319ed
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-10-19 11:11:04 +0600

    shortend array

--- array/array1.go
+++ array/array1.go
@@ -4,5 +4,10 @@ import "fmt"
 
 func main() {
 	var arr [5]int
+	arr[0] = 20
+	arr[1] = 30
+	arr[2] = 50
+	arr[3] = 10
+	arr[4] = 40
 	fmt.Println("Array:", arr)
 }
---
+++ array/shortenArray.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := [5]int{10, 20, 30, 40, 50}
+	fmt.Println("Array elements are:", arr)
+}
