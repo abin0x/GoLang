437bdefa87b28c58d11855421cfef224ab57b840
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-10-19 20:47:48 +0600

    pointer address pass in array

--- pointer/pointer1.go
+++ pointer/pointer1.go
@@ -4,6 +4,8 @@ func main() {
 	x := 20
 	p := &x
 	println("Value of x is:", x)
+	*p = 50
+	println("Value of x is:", x)
 	println("Address of x is: ", p)
 	println("Value at address p is:", *p)
 }
---
+++ pointer/pointer2.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func print(numbers *[5]int) {
+
+	fmt.Println(numbers)
+}
+
+func main() {
+	arr := [5]int{1, 2, 3, 4, 5}
+	print(&arr)
+}
