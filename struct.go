b077a720b4500dc1b4bae6712ac645cf755b1f9a
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-10-19 09:42:19 +0600

    Struct 2

--- struct.go
+++ struct.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type User struct {
 	Name  string
 	Email string
@@ -13,4 +15,18 @@ func main() {
 		Age:   30,
 		Email: "mahmudu@example.com",
 	}
-}
+	fmt.Println("User 1:", user1)
+	fmt.Println("Name: ", user1.Name)
+	fmt.Println("Email: ", user1.Email)
+	fmt.Println("Age: ", user1.Age)
+
+
+	user2= User {
+		Name:  "John",
+		Age:   25,
+		Email: "john@example.com",
+	}
+	fmt.Println("User 2:", user2)
+	fmt.Println("Name: ", user2.Name)
+	fmt.Println("Email: ", user2.Email)
+	fmt.Println("Age: ", user2.Age)
