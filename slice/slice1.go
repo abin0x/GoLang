f42baf1cddd22ab6caec5bcf3e412175364997e9
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-10-20 08:00:37 +0600

    slice to slice copy

--- slice/slice1.go
+++ slice/slice1.go
@@ -5,6 +5,10 @@ import "fmt"
 func main() {
 	arr := [6]string{"This", "is", "a", "Go", "interview", "question"}
 	fmt.Println(arr)
+
 	s := arr[1:4]
 	fmt.Println(s)
+
+	s1 := s[1:2]
+	fmt.Println(s1)
 }
