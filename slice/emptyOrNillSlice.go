f32fc29934033880d28bf8a767d918c31b27e3df
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-10-20 09:07:38 +0600

    underlying slice

--- slice/emptyOrNillSlice.go
+++ slice/emptyOrNillSlice.go
@@ -4,11 +4,20 @@ import "fmt"
 
 func main() {
 
-	var s []int //nil slice
-	s = append(s, 100)
-	s = append(s, 200)
-	s = append(s, 300)
-	fmt.Println(s)
-	fmt.Println(len(s))
-	fmt.Println(cap(s))
+	var x []int //nil slice
+	x = append(x, 100)
+	x = append(x, 200)
+	x = append(x, 300)
+	y := x
+	x = append(x, 400)
+	y = append(y, 500)
+	x[0] = 111
+	fmt.Println(x)
+	fmt.Println(y)
 }
+
+// slice underlying array will be created when we append value to nil slice
+// when we append value to nil slice, new underlying array will be created and slice will point to that array
+//rule of underlying array :
+// when 1024 => 100% increase
+// after 1024 => 25% increase
