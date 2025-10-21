64435806cf96e0e77cc60fe98098f341a95c21fc
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-10-21 16:56:49 +0600

    sp and bsdfdspdfffdsfgdd

--- spAndBp/spandBp.go
+++ spAndBp/spandBp.go
@@ -18,3 +18,4 @@ func main() {
 //here the code is simple enough to set breakpoints and step through the execution to observe variable values and function calls.
 //You can set breakpoints on the main function and the add function to inspect the flow of execution.
 //You can also step into the add function to see how the addition is performed and verify the values of x and y during the function call.
+//for example, you can set a breakpoint at the start of the main function to see when the program begins execution.
