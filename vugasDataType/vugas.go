8fc06ef81d7735c088d533ccfd345e90b22b0df3
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-10-23 09:50:27 +0600

    uint

--- vugasDataType/vugas.go
+++ vugasDataType/vugas.go
@@ -7,7 +7,9 @@ func main() {
 	fmt.Println("Vugas Data Type Example")
 
 	var vagus int8 = 127
+	var x uint16 = 65535
 
-	fmt.Println("Vugas data value is: ", vagus)
+	fmt.Println("Vagas data value is: ", vagus)
+	fmt.Println("X data value is: ", x)
 
 }
