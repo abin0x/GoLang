5e62bf87fde85dc2ead61b1475212d198f352640
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-10-23 11:04:00 +0600

    defer core

--- defer/defer2.go
+++ defer/defer2.go
@@ -2,13 +2,40 @@ package main
 
 import "fmt"
 
-func sum(a int, b int) (result int) {
-	result = a + b
+// func sum(a int, b int) (result int) {
+// 	result = a + b
+// 	return
+// }
+func calculate() (result int) {
+	fmt.Println("First", result) // result is 0 here
+	show := func() {
+		result = result + 5
+		fmt.Println("Second", result) // result is 15 here
+	}
+	defer show()
+	result = 10
+	fmt.Println("Third", result) // result is 10 here
 	return
 }
 
-func main() {
+func calc() int {
+	result := 0
+	fmt.Println("First", result) // result is 0 here
+	show := func() {
+		result = result + 5
+		fmt.Println("Second", result) // result is 15 here
+	}
+	defer show()
+	result = 10
+	fmt.Println("Third", result) // result is 10 here
+	return result
+}
 
-	res := sum(45, 55)
-	fmt.Println("Result:", res)
+func main() {
+	fmt.Println("------------------------1st call----------------------------------")
+	a := calculate()
+	fmt.Println("Result from calculate:", a)
+	fmt.Println("------------------------2nd call----------------------------------")
+	b := calc()
+	fmt.Println("Result from calc:", b)
 }
