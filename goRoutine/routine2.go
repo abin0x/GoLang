3eb64fcb054e0d01bb8c86ffe4be60032fc81507
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-10-26 19:24:46 +0600

    gor loop diye run

--- goRoutine/routine2.go
+++ goRoutine/routine2.go
@@ -5,13 +5,15 @@ import (
 	"time"
 )
 
+func PrintHello(i int) {
+	fmt.Printf("Hello, World! %d\n", i)
+}
+
 func main() {
 	for i := 1; i <= 8; i++ {
-		go func(n int) {
-			fmt.Printf("Hello, World! %d\n", n)
-		}(i)
+		go PrintHello(i)
 	}
+
 	time.Sleep(5 * time.Second)
 	fmt.Println("Main Function Ends")
-
 }
