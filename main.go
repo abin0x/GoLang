a54e2e3e2d3f092810d318c7d17f2b0145d8eb2f
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-10-27 22:13:27 +0600

    routine

--- main.go
+++ main.go
@@ -17,4 +17,9 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/hello", helloHandler)
 	mux.HandleFunc("/about", aboutHandler)
+	fmt.Println("Starting server on :3000")
+	err := http.ListenAndServe(":3000", mux)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
