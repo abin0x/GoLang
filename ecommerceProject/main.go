2a77737711c66919af9a93a02d3500ce42869800
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-10-28 09:07:02 +0600

    images udpated

--- ecommerceProject/main.go
+++ ecommerceProject/main.go
@@ -30,7 +30,9 @@ var productList []Product
 
 // getProductsHandler handles GET requests to the /products endpoint
 func getProductsHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet { // or use r.Method != "GET"  only GET requests allowed
+	w.Header().Set("Access-Control-Allow-Origin", "*") // Allow requests from any origin
+	w.Header().Set("Content-Type", "application/json") // Set content type to JSON
+	if r.Method != http.MethodGet {                    // or use r.Method != "GET"  only GET requests allowed
 		http.Error(w, "Method not allowed, please use GET", http.StatusMethodNotAllowed) //or use status code 400,syntax is  http.Error(w, "Method not allowed", 400)
 		return
 	}
@@ -58,42 +60,42 @@ func init() {
 		Title:       "Laptop",
 		Description: "A high-performance laptop",
 		Price:       999.99,
-		ImgUrl:      "http://example.com/laptop.jpg",
+		ImgUrl:      "https://techterms.com/img/xl/laptop_586.png",
 	}
 	prd2 := Product{
 		ID:          2,
 		Title:       "Smartphone",
 		Description: "A latest model smartphone",
 		Price:       699.99,
-		ImgUrl:      "http://example.com/smartphone.jpg",
+		ImgUrl:      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTYAx3hqK3b3i-JGJlw7nhqqrccJtkMnNv8Q&s",
 	}
 	prd3 := Product{
 		ID:          3,
 		Title:       "Headphones",
 		Description: "Noise-cancelling headphones",
 		Price:       199.99,
-		ImgUrl:      "http://example.com/headphones.jpg",
+		ImgUrl:      "https://cdn.mos.cms.futurecdn.net/HMCWShKerkfeNQmYYhE3p7.jpg",
 	}
 	prd4 := Product{
 		ID:          4,
 		Title:       "Smartwatch",
 		Description: "A smartwatch with various features",
 		Price:       299.99,
-		ImgUrl:      "http://example.com/smartwatch.jpg",
+		ImgUrl:      "https://istarmax.com/wp-content/uploads/2024/04/Starmax-Product-Range-Summer-2024-2.png",
 	}
 	prd5 := Product{
 		ID:          5,
 		Title:       "Tablet",
 		Description: "A lightweight tablet",
 		Price:       399.99,
-		ImgUrl:      "http://example.com/tablet.jpg",
+		ImgUrl:      "https://p2-ofp.static.pub/fes/cms/2021/10/28/juqs65pgl1gh3dysi7yv1tnvtsiqva364946.png",
 	}
 	prd6 := Product{
 		ID:          6,
 		Title:       "Camera",
 		Description: "A digital camera",
 		Price:       499.99,
-		ImgUrl:      "http://example.com/camera.jpg",
+		ImgUrl:      "https://www.gearpatrol.com/wp-content/uploads/sites/2/2023/09/best-vintage-film-cameras-refresh-lead-650de2dd54d04-jpg.webp",
 	}
 	// productList = []Product{prd1, prd2, prd3, prd4, prd5, prd6}
 	productList = append(productList, prd1, prd2, prd3, prd4, prd5, prd6)
