9b37bbd5e83fe18bfebba86dcc917ebebbf7c167
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-10-28 09:46:10 +0600

    route add

--- ecommerceProject/main.go
+++ ecommerceProject/main.go
@@ -71,6 +71,7 @@ func main() {
 	mux.HandleFunc("/hello", helloHandler)
 	mux.HandleFunc("/about", aboutHandler)
 	mux.HandleFunc("/products", getProductsHandler)
+	mux.HandleFunc("/createproduct", createproductHandler)
 	fmt.Println("Starting server on :8080")
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
