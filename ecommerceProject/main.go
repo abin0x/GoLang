fc378e67ff356f494c4415a9e8a439bc87bc675a
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-10-28 21:02:09 +0600

    Code reflector done

--- ecommerceProject/main.go
+++ ecommerceProject/main.go
@@ -68,7 +68,6 @@ func createproductHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleCors(w http.ResponseWriter) {
-
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
