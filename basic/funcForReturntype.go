2bb3519bb99e97d69b890d3343ca1aecc9b2b9a3
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2026-02-07 12:16:54 +0600

    Add calculator function and print results

--- basic/funcForReturntype.go
+++ basic/funcForReturntype.go
@@ -1,14 +1,25 @@
 package main
 
 import "fmt"
-func add(x int,y int) int {
-	result:=x+y
+
+func add(x int, y int) int {
+	result := x + y
+	fmt.Println("The sum of", x, "and", y, "is:", result)
 	return result
+}
+func calculator(x int, y int, z int) (sum int, product int) {
+	sum = x + y + z
+	product = x * y * z
+	return
 }
 
 func main() {
-	a:=10
-	b:=20
-	sum:= add(a,b)
-	fmt.Println(sum)
+	a := 10
+	b := 20
+	sum := add(a, b)
+	fmt.Println("Returned value from add function:", sum)
+
+	s, p := calculator(a, b, 5)
+	fmt.Println("Sum from calculator function:", s)
+	fmt.Println("Product from calculator function:", p)
 }
