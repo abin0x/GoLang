1c2e08fbeb13846a99656eed0eaa5b8c24514b78
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2026-02-07 10:48:53 +0600

    Refactor ifelse.go to use Scanln for input

--- basic/ifelse.go
+++ basic/ifelse.go
@@ -6,14 +6,18 @@ func main() {
 
 	var name string
 	var age int
-	fmt.Println("Enter your name: ")
-	fmt.Scanf("%s %d", &name, &age)
-	fmt.Println("Enter your age: ")
-	fmt.Scanf("%d", &age)
+
+	fmt.Println("Enter your name:")
+	fmt.Scanln(&name)
+
+	fmt.Println("Enter your age:")
+	fmt.Scanln(&age)
 
 	if age > 20 && name == "Abin" {
 		fmt.Println("You are an adult and your name is Abin")
-	} else if age > 20 {
-		fmt.Println("You are an adult but your name is not Abin")
+	} else if age < 20 && name != "Abin" {
+		fmt.Println("You are a minor and your name is not Abin")
+	} else {
+		fmt.Println("You are either an adult or your name is Abin")
 	}
 }
