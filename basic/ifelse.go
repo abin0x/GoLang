6cae95e2bf474f5d547e932a3e49db9f288290b8
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2026-02-07 10:38:24 +0600

    Fix input prompt and scanf format

--- basic/ifelse.go
+++ basic/ifelse.go
@@ -6,7 +6,10 @@ func main() {
 
 	var name string
 	var age int
+	fmt.Println("Enter your name: ")
 	fmt.Scanf("%s %d", &name, &age)
+	fmt.Println("Enter your age: ")
+	fmt.Scanf("%d", &age)
 
 	if age > 20 && name == "Abin" {
 		fmt.Println("You are an adult and your name is Abin")
